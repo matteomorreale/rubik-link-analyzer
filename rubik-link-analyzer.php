bdf2323f890fe8b2631b4c13f1925952df84df57
matteomorreale <matteo.morreale@gmail.com>
2024-11-15 15:48:33 +0000

    fixed updater

--- rubik-link-analyzer.php
+++ rubik-link-analyzer.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Rubik Link Analyzer
  * Description: Plugin per l'analisi dei link presenti negli articoli WordPress.
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: Matteo Morreale
  */
 
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Definisco una costante per la versione corrente del plugin
-define('RUBIK_LINK_ANALYZER_VERSION', '1.0.1');
+define('RUBIK_LINK_ANALYZER_VERSION', '1.0.4');
 define('RUBIK_LINK_ANALYZER_PLUGIN_FILE', __FILE__);
 
 require_once("admin/updater.php");
